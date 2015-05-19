e47d7cf36278ccf9caab1511d7c55e0b2f7a08a2
Gavin Thompson <gavinathompson@gmail.com>
2015-05-18 22:48:13 -0400

    adding a border body and physics body to prevent ball from disappearing

--- spriteKitSwiftBrickGame/GameScene.swift
+++ spriteKitSwiftBrickGame/GameScene.swift
@@ -10,5 +10,12 @@ let BlockNodeCategoryName = "blockNode"
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
         super.didMoveToView(view)
+        
+        // Create a physics body that borders the screen
+        let borderBody = SKPhysicsBody( edgeLoopFromRect: self.frame)
+        // Set the friction of that physicsBody to 0
+        borderBody.friction = 0
+        // Set physicsBody of scene to borderBody
+        self.physicsBody = borderBody
     }
 }
